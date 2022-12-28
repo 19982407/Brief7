80eff6bdb7dc45a9bc19067893a77ce52bfc2169
Safaâ Faiz <88689147+19982407@users.noreply.github.com>
2022-12-28 09:27:32 +0100

    Update bref7.js

--- Brief7/bref7.js
+++ Brief7/bref7.js
@@ -1,171 +1,146 @@
-document.getElementById("add").addEventListener("click", (e) => {
-  ajouter();
-  if (arr.length != 6) {
-    e.preventDefault();
+const tblProducts = document.querySelector("#tabx")
+const inputName = document.querySelector("#inputName")
+const inputMarque = document.querySelector("#inputMarque")
+const inputPrix = document.querySelector("#inputPrix")
+const inputDate = document.querySelector("#inputDate")
+const selectTypeProduit = document.querySelector("#selectTypeProduit")
+const radioPromotionOui = document.querySelector("#radioPromotionOui")
+const radioPromotionNon = document.querySelector("#radioPromotionNon")
+const modal = document.querySelector("#modal")
+
+let listProducts = [];
+
+function onSave(event) {
+  event.preventDefault();
+
+  if (isValidForm()) {
+    listProducts.push({
+      name: inputName.value,
+      marque: inputMarque.value,
+      prix: inputPrix.value,
+      date: inputDate.value,
+      typeProduit: selectTypeProduit.value,
+      promotion: radioPromotionOui.checked ? 'Oui' : 'Non',
+    })
+  
+    inputName.value = ''
+    inputDate.value = ''
+    inputMarque.value = ''
+    inputPrix.value = ''
+    selectTypeProduit.value = ''
+    radioPromotionOui.checked = false;
+    radioPromotionNon.checked = true;
+  
+    reRenderListProducts();
+  }
+
+}
+
+function isValidForm() {
+  if (inputName.value == '') {
+    document.querySelector("#inputNameError").style.display =  'block';
+    return false;
   } else {
-    id++;
-    arrvalue.push(`<a href="#" id="edite${id}" onclick = "edit(this)" class="btn btn-warning btn-sm edit">Edit</a>
-    <a href="#" onclick="supprimer(this)" id="delete${id}" class="btn btn-danger btn-sm delete">Delete</a>`);
-    save();
-    clearFields();
+    document.querySelector("#inputNameError").style.display =  'none';
   }
-});
-let arr = [];
-let id = 0;
-let arrvalue = [];
-function ajouter() {
-  Namex();
-  marquex();
-  prixx();
-  date();
-  var selectproduit = document.getElementById("produit").value;
-  var radio = document.getElementsByName("inlineRadioOptions");
-
-  let radiovalue;
-  for (let i = 0; i < radio.length; i++) {
-    if (radio[i].checked) {
-      radiovalue = radio[i].value;
-    }
-  }
-
-  if (selectproduit == "") {
-    document.getElementById("selectOblig").innerHTML =
-      "* tu dois choisir un option";
-    document.getElementById("selectOblig").style.color = "red";
+  
+  if (inputMarque.value == '') {
+    document.querySelector("#inputMarqueError").style.display =  'block';
+    return false;
   } else {
-    document.getElementById("selectOblig").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(selectproduit);
+    document.querySelector("#inputMarqueError").style.display =  'none';
   }
-  if (!radio[0].checked && !radio[1].checked) {
-    document.getElementById("radioOblig").innerHTML =
-      "* tu dois choisir un seul option";
-    document.getElementById("radioOblig").style.color = "red";
+
+  if (inputPrix.value == '') {
+    document.querySelector("#inputPrixError").style.display =  'block';
+    return false;
   } else {
-    document.getElementById("radioOblig").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(radiovalue);
+    document.querySelector("#inputPrixError").style.display =  'none';
   }
-}
 
-function Namex() {
-  var nom = document.getElementById("Nom");
-  if (nom.value == "") {
-    document.getElementById("NomOblig").innerHTML = "* le nom est obligatoire";
-    document.getElementById("NomOblig").style.color = "red";
+  if (inputDate.value == '') {
+    document.querySelector("#inputDateError").style.display =  'block';
+    return false;
   } else {
-    document.getElementById("NomOblig").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(nom.value);
+    document.querySelector("#inputDateError").style.display =  'none';
   }
-}
-function date() {
-  var date = document.getElementById("Date");
-  if (date.value == "") {
-    document.getElementById("Date").innerHTML = "* Choisir une date";
-    document.getElementById("Date").style.color = "red";
+
+  if (selectTypeProduit.value == '') {
+    document.querySelector("#selectProduitError").style.display =  'block';
+    return false;
   } else {
-    document.getElementById("Date").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(date.value);
+    document.querySelector("#selectProduitError").style.display =  'none';
   }
+
+  return true;
 }
 
-function marquex() {
-  var marque = document.getElementById("Marque");
-  if (marque.value == "") {
-    document.getElementById("MarqueOblig").innerHTML =
-      "* la Marque est obligatoire";
-    document.getElementById("MarqueOblig").style.color = "red";
-  } else {
-    document.getElementById("MarqueOblig").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(marque.value);
-  }
+function edit(elementIndex) {
+  const [prodcut] = listProducts.splice(elementIndex, 1);
+
+  inputName.value = prodcut.name
+  inputDate.value = prodcut.date
+  inputMarque.value = prodcut.marque
+  inputPrix.value = prodcut.prix
+  selectTypeProduit.value = prodcut.typeProduit
+  radioPromotionOui.checked = prodcut.promotion === 'Oui';
+  radioPromotionNon.checked = prodcut.promotion === 'Non';
+
+  reRenderListProducts();
 }
 
-function prixx() {
-  var prix = document.getElementById("Prix");
-
-  if (prix.value == "") {
-    document.getElementById("PrixOblig").innerHTML =
-      "* le Prix est obligatoire";
-    document.getElementById("PrixOblig").style.color = "red";
-  } else {
-    document.getElementById("PrixOblig").innerHTML = "";
-    arr.push(true);
-    arrvalue.push(prix.value);
-  }
+let elementToDeleteAfterConfirm;
+function supprimer(elementIndex) {
+  elementToDeleteAfterConfirm = elementIndex;
+  modal.style.display = 'grid';
+  return false;
 }
 
-// Save //
-function save() {
-  let tr = document.createElement("tr");
-  tr.setAttribute("id", "tr" + id);
-  document.getElementById("tbl").appendChild(tr);
-  for (let i = 0; i < arrvalue.length; i++) {
-    let td = document.createElement("td");
-    td.setAttribute("id", "td" + id + i);
-    document.getElementById("tr" + id).appendChild(td);
-    document.getElementById("td" + id + i).innerHTML = arrvalue[i];
-  }
-}
-// Clear All Fields
-function clearFields() {
-  document.getElementById("Nom").value = "";
-  document.getElementById("Marque").value = "";
-  document.getElementById("Date").value = "";
-  document.getElementById("Prix").value = "";
-  document.getElementById("produit").value = "";
-  let radio = document.getElementsByName("inlineRadioOptions");
-  for (let i = 0; i < radio.length; i++) {
-    radio[i].checked = false;
-  }
-  arr = [];
-  arrvalue = [];
-}
-/////// Modal/////
-function modal() {
-  document.getElementById("modal").style.display = "grid";
-}
-document.getElementById("annuler").onclick = function () {
-  document.getElementById("modal").style.display = "none";
-};
-function supprimer(sup) {
-  modal();
-  document.getElementById("supprimer").onclick = function () {
-    sup.closest("tr").remove();
-    document.getElementById("modal").style.display = "none";
-  };
-}
-//////// Edit //////////
+function confirmDelete() {
+  modal.style.display = 'none';
+  listProducts.splice(elementToDeleteAfterConfirm, 1);
 
-function edit(edit) {
-  let edite = document.getElementById("edit");
-  let add = document.getElementById("add");
-  edite.style.display = "block";
-  add.style.display = "none";
-  let table = edit.closest("tr");
-  let tdata = table.querySelectorAll("td");
-  let tabledata = [];
-  let inputdata = [];
-  tdata.forEach((element) => tabledata.push(element.innerHTML));
-  let input = document.querySelectorAll("form input,select");
-  input.forEach((element) => inputdata.push(element));
-  for (let i = 0; i < tabledata.length - 2; i++) {
-    inputdata[i].value = tabledata[i];
-  }
-  edite.onclick = function () {
-    ajouter();
-    if (arr.length != 6) {
-      e.preventDefault();
-    } else {
-      for (let i = 0; i < arrvalue.length; i++) {
-        tdata[i].innerHTML = arrvalue[i];
-      }
-      clearFields();
-      edite.style.display = "none";
-      add.style.display = "block";
-    }
-  };
+  reRenderListProducts();
 }
+
+function cancelDelete() {
+  modal.style.display = 'none';
+}
+
+function createColumn(value) {
+  var column = document.createElement('td');
+  column.appendChild(document.createTextNode(value));
+  return column;
+}
+
+function createButton(index, name, action, clazz) {
+  var btn = document.createElement('a');
+  btn.setAttribute('class', 'btn btn-sm ' + clazz);
+  btn.setAttribute('onclick', action);
+  btn.appendChild(document.createTextNode(name));
+  return btn;
+}
+
+function reRenderListProducts() {
+  const rows = listProducts.map((proudct, index) => {
+    const row = document.createElement('tr');
+    row.appendChild(createColumn(proudct.name));
+    row.appendChild(createColumn(proudct.marque));
+    row.appendChild(createColumn(proudct.prix));
+    row.appendChild(createColumn(proudct.date));
+    row.appendChild(createColumn(proudct.typeProduit));
+    row.appendChild(createColumn(proudct.promotion));
+
+    var btnColumn = document.createElement('td');
+    btnColumn.appendChild(createButton(index, 'Edit', `return edit(${index})`, 'btn-warning'));
+    btnColumn.appendChild(createButton(index, 'Delete', `return supprimer(${index})`, 'mx-2 btn-danger'));
+    row.appendChild(btnColumn);
+
+    return row;
+  })
+
+  tblProducts.replaceChildren(...rows)
+}
+
+reRenderListProducts();
+
